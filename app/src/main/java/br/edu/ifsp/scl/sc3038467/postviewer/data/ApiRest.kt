11bb5f325af6c1f0c9b194529eacb2e59a8ebf13
Murilo Corrêa <murilosao06@gmail.com>
2026-06-18 15:53:13 -0300

    feat: configura cliente retrofit com url base e conversor gson

--- app/src/main/java/br/edu/ifsp/scl/sc3038467/postviewer/data/ApiRest.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038467/postviewer/data/ApiRest.kt
@@ -2,6 +2,8 @@ package br.edu.ifsp.scl.sc3038467.postviewer.data
 
 import br.edu.ifsp.scl.sc3038467.postviewer.model.Comment
 import br.edu.ifsp.scl.sc3038467.postviewer.model.Post
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -11,4 +13,17 @@ interface ApiService {
 
     @GET("posts/{id}/comments")
     suspend fun buscarComentarios(@Path("id") postId: Int): List<Comment>
+}
+
+
+object ClienteRetrofit {
+    private const val URL_BASE = "https://jsonplaceholder.typicode.com/"
+
+    val api: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(URL_BASE)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
 }
