8e736b2715b1d30f3b4f210c78aa5ee716c08229
Murilo Corrêa <murilosao06@gmail.com>
2026-06-18 15:07:40 -0300

    feat: configura instancia do banco de dados room

--- app/src/main/java/br/edu/ifsp/scl/sc3038467/postviewer/data/BancoDeDados.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038467/postviewer/data/BancoDeDados.kt
@@ -1,11 +1,14 @@
 package br.edu.ifsp.scl.sc3038467.postviewer.data
 
+import android.content.Context
 import androidx.room.Dao
+import androidx.room.Database
 import androidx.room.Entity
 import androidx.room.Insert
 import androidx.room.PrimaryKey
 import androidx.room.Query
-
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 
 @Entity(tableName = "tabela_comentarios_locais")
@@ -22,4 +25,26 @@ interface ComentarioDao {
 
     @Query("SELECT * FROM tabela_comentarios_locais WHERE postId = :postId")
     suspend fun buscarComentariosDoPost(postId: Int): List<ComentarioLocal>
+}
+
+@Database(entities = [ComentarioLocal::class], version = 1, exportSchema = false)
+abstract class AppBancoDeDados : RoomDatabase() {
+    abstract fun comentarioDao(): ComentarioDao
+
+    companion object {
+        @Volatile
+        private var INSTANCIA: AppBancoDeDados? = null
+
+        fun obterInstancia(context: Context): AppBancoDeDados {
+            return INSTANCIA ?: synchronized(this) {
+                val instancia = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppBancoDeDados::class.java,
+                    "postviewer_banco_dados"
+                ).build()
+                INSTANCIA = instancia
+                instancia
+            }
+        }
+    }
 }
