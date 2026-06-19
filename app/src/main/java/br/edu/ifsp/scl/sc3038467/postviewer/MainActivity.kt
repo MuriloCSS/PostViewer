e64daecd6773d5125be485290d4b48f384b65e99
Murilo Corrêa <murilosao06@gmail.com>
2026-06-18 23:26:46 -0300

    feat: configura MainActivity como ponto de entrada e inicia o fluxo de navegacao

--- app/src/main/java/br/edu/ifsp/scl/sc3038467/postviewer/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038467/postviewer/MainActivity.kt
@@ -3,45 +3,16 @@ package br.edu.ifsp.scl.sc3038467.postviewer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import br.edu.ifsp.scl.sc3038467.postviewer.ui.theme.PostViewerTheme
+import br.edu.ifsp.scl.sc3038467.postviewer.ui.AppNavegacao
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            PostViewerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            PostViewerTheme() {
+                AppNavegacao()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PostViewerTheme {
-        Greeting("Android")
     }
 }
